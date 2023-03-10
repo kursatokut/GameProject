1974fd494aa1405956cfc391415ddd6b4321513a
kursatokut <kursatokut@gmail.com>
2023-03-10 10:17:26 +0300

    Add project files.

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidation
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 2001 && gamer.FirstName == "Kürşat" &&
+             gamer.LastName == "Okut" && gamer.IdentityNumber == 123456789)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
